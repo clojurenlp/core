a14e19e4747322ee1265c74e25890f40768d1841
Cory Giles <mail@corygil.es>
2015-02-17 16:24:47 -0600

    Remove dependency on lein-exec

--- project.clj
+++ project.clj
@@ -5,5 +5,4 @@
                  [edu.stanford.nlp/stanford-corenlp "3.3.1"
                   :classifier "models"]
                  [org.clojure/data.json "0.2.4"]   
-                 [cc.artifice/loom "0.1.3"]]
-  :plugins [[lein-exec "0.3.2"]])
+                 [cc.artifice/loom "0.1.3"]])
