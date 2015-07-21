fa607198bc826db3acc0efebb561e15657010617
Damien Stanton <damien@damienkstanton.com>
2015-07-21 13:57:07 -0400

    I am back to working on this. Updated dependencies

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject es.corygil/corenlp "3.3.1-r2-SNAPSHOT"
   :description "Clojure wrapper for the Stanford CoreNLP tools."
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [edu.stanford.nlp/stanford-corenlp "3.3.1"]
                  [edu.stanford.nlp/stanford-corenlp "3.3.1"
                   :classifier "models"]
-                 [org.clojure/data.json "0.2.4"]   
-                 [cc.artifice/loom "0.1.3"]])
+                 [org.clojure/data.json "0.2.6"]   
+                 [aysylu/loom "0.5.4"]])
