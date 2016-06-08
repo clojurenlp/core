b22ac8b69b6a5dda1e1ac56d18aacf477e263783
Damien Stanton <damien@blinkanalytics.com>
2016-06-08 11:59:50 -0400

    update deps

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject corenlp "3.3.2"
+(defproject corenlp "4.0.0a"
   :description "Clojure wrapper for the Stanford CoreNLP tools."
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [edu.stanford.nlp/stanford-corenlp "3.5.2"]
-                 [edu.stanford.nlp/stanford-corenlp "3.5.2"
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [edu.stanford.nlp/stanford-corenlp "3.6.0"]
+                 [edu.stanford.nlp/stanford-corenlp "3.6.0"
                   :classifier "models"]
-                 [org.clojure/data.json "0.2.6"]   
-                 [aysylu/loom "0.5.4"]])
+                 [org.clojure/data.json "0.2.6"]
+                 [aysylu/loom "0.6.0"]])
