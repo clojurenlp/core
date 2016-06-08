0c5408095aab73b3f2d2312c5d653a193133a8da
Damien Stanton <damien@blinkanalytics.com>
2016-06-08 12:10:06 -0400

    Change version to match CoreNLP

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject corenlp "4.0.0a"
+(defproject corenlp "3.6.0a"
   :description "Clojure wrapper for the Stanford CoreNLP tools."
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [edu.stanford.nlp/stanford-corenlp "3.6.0"]
