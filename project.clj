dd9d54a34f5c39cdc0fc726bb37fa312fbdbb48e
Damien Stanton <damien@blinkanalytics.com>
2016-06-09 11:48:10 -0400

    only build master, add lein auto

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
                  [edu.stanford.nlp/stanford-corenlp "3.6.0"
                   :classifier "models"]
                  [org.clojure/data.json "0.2.6"]
-                 [aysylu/loom "0.6.0"]])
+                 [aysylu/loom "0.6.0"]]
+  :plugins [[lein-auto "0.1.2"]])
