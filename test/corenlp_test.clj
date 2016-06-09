7e588fc84f69d119698542c8736f391e1d20fbbd
Damien Stanton <damien@blinkanalytics.com>
2016-06-09 11:57:45 -0400

    remove tests for now, rewriting

--- test/corenlp_test.clj
+++ test/corenlp_test.clj
@@ -2,32 +2,4 @@
   (:require [corenlp :as corenlp]
             [clojure.test :refer :all]))
 
-
-(deftest tokenize-test
-  (testing "tokenize"
-    (is (= (map corenlp/word ["Petunia" "is" "my" "cat" "."])
-           (corenlp/tokenize "Petunia is my cat.")))))
-
-
-(deftest split-sentences-test
-  (testing "split-sentences"
-    (let [sent-tokens (corenlp/split-sentences
-                       "Petunia is my cat. She is a DSH.")]
-      (is (= [["Petunia" "is" "my" "cat" "."] ["She" "is" "a" "DSH" "."]]
-             sent-tokens)))))
-
-
-(deftest pos-tagging-test
-  (testing "pos-tag"
-    (let [tagged-words (corenlp/pos-tag
-                        (corenlp/tokenize "Petunia is my cat."))]
-      (is (= (map #(edu.stanford.nlp.ling.TaggedWord. %1 %2)
-                  ["Petunia" "is" "my" "cat" "."]
-                  ["NNP" "VBZ" "PRP$" "NN" "."])
-             tagged-words)))))
-
-
-(deftest parse-test
-  (testing "parse"
-    (is (= "(ROOT (S (NP (NNP Petunia)) (VP (VBZ is) (NP (PRP$ my) (NN cat))) (. .)))"
-           (str (corenlp/parse (corenlp/tokenize "Petunia is my cat.")))))))
+;; TODO rewrite test suite
