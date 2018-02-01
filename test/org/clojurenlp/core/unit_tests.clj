2b224e0fcc0ebdd4c4c0d969b6925ea3d2710d73
Leon Talbot <leontalbot@gmail.com>
2018-01-31 21:09:05 -0500

    new tokenization test and another pos test

--- test/org/clojurenlp/core/unit_tests.clj
+++ test/org/clojurenlp/core/unit_tests.clj
@@ -1,6 +1,27 @@
 (ns org.clojurenlp.core.unit-tests
-  (:require [org.clojurenlp.core :as corenlp]
-            [clojure.test :refer :all]))
+  (:require [org.clojurenlp.core :as core]
+            [clojure.test :refer [deftest is]]))
 
-(deftest tag
-  (is (= "JJ" (->> "Short and sweet." corenlp/tokenize corenlp/pos-tag first .tag))))
+(deftest tokenization-test
+  (is
+   (= '({:token "This", :start-offset 0, :end-offset 4}
+        {:token "is", :start-offset 5, :end-offset 7}
+        {:token "a", :start-offset 8, :end-offset 9}
+        {:token "simple", :start-offset 10, :end-offset 16}
+        {:token "sentence", :start-offset 17, :end-offset 25}
+        {:token ".", :start-offset 25, :end-offset 26})
+      (core/tokenize "This is a simple sentence."))))
+
+(deftest pos-test
+  (is
+   (= "JJ"
+      (->> "Short and sweet."
+           core/tokenize
+           core/pos-tag
+           first
+           .tag)))
+  (is (= '("JJ" "CC" "JJ" ".")
+         (->> "Short and sweet."
+              core/tokenize
+              core/pos-tag
+              (map #(.tag %))))))
